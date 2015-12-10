af5fe565f75fd6ec43340c5c9b6da336d2f6329f
nanflower <xc8609@126.com>
2015-12-10 22:30:37 +0800

    save cookie

--- demo.py
+++ demo.py
@@ -2,13 +2,9 @@
 import urllib2
 import cookielib
 
-cookie = cookielib.CookieJar()
-#利用urllib2库的HTTPCookieProcessor对象来创建cookie处理器
+filename = 'cookie.txt'
+cookie = cookielib.MozillaCookieJar(filename)
 handler = urllib2.HTTPCookieProcessor(cookie)
-#通过handler来构建opener
 opener = urllib2.build_opener(handler)
-#此处的open方法同urllib2的urlopen方法，也可以传入request
-response = opener.open('http://www.baidu.com')
-for item in cookie:
-	print 'Name = ' + item.name
-	print 'Value = ' + item.value
+response = opener.open("http://www.baidu.com")
+cookie.save(ignore_discard=True, ignore_expires=True)
