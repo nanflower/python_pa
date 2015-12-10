fdb992a98ca7b8b8a525dd47a83044fb75002a8a
nanflower <xc8609@126.com>
2015-12-10 22:36:21 +0800

    get cookie from file

--- demo.py
+++ demo.py
@@ -2,9 +2,9 @@
 import urllib2
 import cookielib
 
-filename = 'cookie.txt'
-cookie = cookielib.MozillaCookieJar(filename)
-handler = urllib2.HTTPCookieProcessor(cookie)
-opener = urllib2.build_opener(handler)
-response = opener.open("http://www.baidu.com")
-cookie.save(ignore_discard=True, ignore_expires=True)
+cookie = cookielib.MozillaCookieJar()
+cookie.load('cookie.txt', ignore_discard=True, ignore_expires=True)
+req = urllib2.Request("http://www.baidu.com")
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
+response = opener.open(req)
+print response.read()
